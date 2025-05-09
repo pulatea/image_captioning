34ce111a6d6881e7a4500b563481382fc7b61327
Pula, Tea (STUDENTS) <teaapulaa@gmail.com>
2025-05-09 16:42:12 +0200

    model 4

--- assignment_2/models/model_4.py
+++ assignment_2/models/model_4.py
@@ -1,19 +1,43 @@
+from einops import rearrange, pack
 from models.base import BaseModel, BaseImageEncoder, BaseCaptionGenerator
+import torch.nn
+import torch.nn.functional as F
 
 
 class Model(BaseModel):
     """Base class for all models."""
-    def __init__(self, vocabulary):
+
+    def __init__(self, embedding_dim, num_layers, vocabulary):
         super().__init__(vocabulary=vocabulary)
+
+        self.embedding_dim = embedding_dim
+        self.num_layers = num_layers
+
+        self.image_encoder = ImageEncoder(embedding_dim=self.embedding_dim)
+        self.caption_generator = CaptionGenerator(vocabulary_size=len(self.vocabulary),
+                                                  embedding_dim=self.embedding_dim,
+                                                  hidden_dim=self.embedding_dim,
+                                                  num_layers=self.num_layers)
 
 
 class ImageEncoder(BaseImageEncoder):
-    def __init__(self):
+    def __init__(self, embedding_dim):
         super().__init__()
+
+        self.embedding_dim = embedding_dim
+
+        # loading pretrained DINOv2 model
+        self.dinov2 = torch.hub.load('facebookresearch/dinov2', 'dinov2_vits14')
+        self.model_dim = torch.nn.Linear(self.dinov2.embed_dim, self.embedding_dim)
 
     def freeze(self):
         """Sets the requires_grad parameter to False for some model parameters."""
-        raise NotImplementedError
+        # freezing the backbone so we use it only as it is (pretrained) without further finetuning
+        for param in self.dinov2.parameters():
+            param.requires_grad = False
+        for param in self.model_dim.parameters():
+            param.requires_grad = True  # enable it for decoding
+        # raise NotImplementedError
 
     def forward(self, image):
         """Forward method.
@@ -22,20 +46,46 @@ class ImageEncoder(BaseImageEncoder):
 
         :return: encoded image (torch.tensor) of the shape [batch_size, *]
         """
-        raise NotImplementedError
+        # resizing to (224, 224) - default input size for ViT models
+        # bilinear interpolation gives us a smooth and common image resample
+        updated_image = F.interpolate(image, size=[224, 224], mode='bilinear', align_corners=False)
+
+        intermediate_features = self.dinov2.get_intermediate_layers(updated_image,
+                                                                    n=1,
+                                                                    reshape=True,
+                                                                    return_class_token=True,
+                                                                    norm=True)
+        _, dino_cls_t = intermediate_features[-1]
+
+        # project cls token (image encoding) to have a compatible dimension with the decoder
+        return self.model_dim(dino_cls_t)
 
 
 class CaptionGenerator(BaseCaptionGenerator):
-    def __init__(self, vocabulary_size):
-        super().__init__()
+    def __init__(self, vocabulary_size, embedding_dim, hidden_dim, num_layers):
+        super().__init__(vocabulary_size)
+        self.embedding_dim = embedding_dim
+        self.embedding = torch.nn.Sequential(torch.nn.Embedding(num_embeddings=self.vocabulary_size,
+                                                                embedding_dim=self.embedding_dim),
+                                             torch.nn.Dropout(0.5))
+        self.hidden_dim = hidden_dim
+        self.num_layers = num_layers
+        self.rnn = torch.nn.LSTM(
+            input_size=embedding_dim,
+            hidden_size=hidden_dim,
+            num_layers=num_layers,
+            batch_first=True,
+            bias=True
+        )
 
-        self.vocabulary_size = vocabulary_size
+        self.to_logits = torch.nn.Linear(in_features=self.hidden_dim, out_features=self.vocabulary_size)
 
     def freeze(self):
         """Sets the requires_grad parameter to False for some model parameters."""
-        raise NotImplementedError
+        # raise NotImplementedError
+        pass
 
-    def forward(self, encoded_image, caption_indices, *args):
+    def forward(self, encoded_image, caption_indices, hidden_state=None):
         """Forward method.
 
         :param encoded_image: torch.tensor of the shape [batch_size, *] or None
@@ -46,7 +96,20 @@ class CaptionGenerator(BaseCaptionGenerator):
             where: logits is the torch.tensor of the shape [batch_size, vocabulary_size, sequence_length]
                    indices is the torch.tensor of the shape [batch_size, sequence_length]
         """
-        raise NotImplementedError
+        # now embeddings are only word embeddings, and not as before [image_encoded, embedding1, embedding2, ..., embeddingN]
+        embeddings = self.embedding(caption_indices)
+
+        if hidden_state is None and encoded_image is not None:
+            h_0 = torch.tanh(encoded_image).unsqueeze(0).repeat(self.num_layers, 1, 1).contiguous()
+            c_0 = torch.zeros_like(h_0).contiguous()
+            hidden_state = (h_0, c_0)
+
+        output, hidden_state = self.rnn(input=embeddings, hx=hidden_state)
+        logits = self.to_logits(output)
+        logits = rearrange(logits, 'batch sequence_length vocabulary_size -> batch vocabulary_size sequence_length')
+
+        return {'logits': logits, 'indices': logits.argmax(dim=-2), 'hidden_state': hidden_state}
+        # raise NotImplementedError
 
     def generate_caption_indices(self, encoded_image, sos_token_index, eos_token_index, max_length):
         """Generates caption indices like torch.tensor([1, 23, 5, 8, 2]).
@@ -58,4 +121,23 @@ class CaptionGenerator(BaseCaptionGenerator):
 
         :return: caption indices (list of the length <= max_length)
         """
-        raise NotImplementedError
+
+        # start from the word embeddings (start of sentence)
+        caption_indices = [sos_token_index]
+
+        predicted_index = torch.LongTensor([sos_token_index]).to(encoded_image.device).unsqueeze(0)  # [1, 1]
+
+        output = self.forward(encoded_image, caption_indices=predicted_index, hidden_state=None)
+        for _ in range(max_length):
+            predicted_index = output['indices']
+
+            caption_indices.append(predicted_index.item())
+            if predicted_index.item() == eos_token_index:
+                break
+
+            output = self.forward(encoded_image=None,
+                                  caption_indices=predicted_index,
+                                  hidden_state=output['hidden_state'])
+
+        return caption_indices
+        # raise NotImplementedError
